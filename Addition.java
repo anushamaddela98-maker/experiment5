034bc4156c1d2af493c4b087e90b365fdb6ba586
anushamaddela98-maker <anushamaddela98@gmail.com>
2026-08-11 12:25:18 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,1 +1,15 @@
-a
+pipeline{
+  agent any
+  stages{
+    stage('compile'){
+      steps{
+        sh 'javac HelloWorld.java'
+      }
+    }
+    stage('Run'){
+      steps{
+        sh 'java HelloWorld'
+      }
+    }
+  }
+}
